f3248b28a9d286117c14ba888f3e6d444d2f7e9f
mackenziehamlett <mackenzie@hamlett.org>
2022-10-13 14:38:58 -0500

    removed old layout to make new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,13 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route,
+  Link
+} from "react-router-dom";
 import { 
+  Box,
+  Flex,
   ChakraProvider, 
   Heading,
   Center,
@@ -6,49 +15,38 @@ import {
   InputGroup, 
   InputLeftElement,
   Stack,
-   } from '@chakra-ui/react'
-import {
+  extendTheme,
   List,
   ListItem,
   ListIcon,
   OrderedList,
   UnorderedList,
-} from '@chakra-ui/react'
-import {SearchIcon} from '@chakra-ui/icons'
-import { Select } from '@chakra-ui/react'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+  SearchIcon,
+  Select 
+  } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    blue: '#FF0000',
+    tan: '#F3EACO',
+    orange: '#DC9750',
+    red: '#922C40'
+  },
+}
+
+const theme = extendTheme({colors})
 
 function App() {
   return (
-    <ChakraProvider>
-      <Stack spacing={4}>
-        <Center>
-          <Heading as='h3' size='lg'>
-            RESme
-          </Heading>
-        </Center>
-        <InputGroup>
-            <InputLeftElement
-            pointerEvents='none'
-            children={<SearchIcon color='gray.400' />}/>
-            <Input placeholder='Search' />
-          </InputGroup>
-
-          <Select placeholder='Filter and Sort'>
-            <option value='option1'>Artificial Intelligence</option>
-            <option value='option2'>Computer Vision & Pattern Recognition</option>
-            <option value='option3'>Distributed Parallel & Cluster Computing</option>
-          </Select>
-
-          <UnorderedList>
-            <ListItem>A random link 1</ListItem>
-            <ListItem>A random link 2</ListItem>
-            <ListItem>A random link 3</ListItem>
-            <ListItem>A random link 4</ListItem>
-          </UnorderedList>
-      </Stack>
-    </ChakraProvider>
-    
+    <Router>
+      <ChakraProvider theme={theme}>
+        <Flex minWidth='max-content' alignItems='center' gap='2'>
+          <Box p='2'>
+            <Heading>RESme</Heading>
+          </Box>
+        </Flex>
+      </ChakraProvider>
+    </Router>
   );
 }
 
