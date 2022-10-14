4a707011c47987ed7cb85125e41d625b2d3caca6
mackenziehamlett <mackenzie@hamlett.org>
2022-10-14 12:44:40 -0500

    fixed bullshit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import LandingPage from "./LandingPage"
+import ReactDOM from "react-dom";
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch, 
+  BrowserRouter as Router, 
+  Routes,
   Route,
-  Link
+  Link,
+  withRouter
 } from "react-router-dom";
 import { 
   Box,
@@ -22,12 +25,16 @@ import {
   OrderedList,
   UnorderedList,
   SearchIcon,
-  Select 
+  Select, 
+  ButtonGroup,
+  LinkBox,
+  Spacer,
+  Button
   } from '@chakra-ui/react'
 
 const colors = {
   brand: {
-    blue: '#FF0000',
+    blue: '#1E2650',
     tan: '#F3EACO',
     orange: '#DC9750',
     red: '#922C40'
@@ -38,15 +45,46 @@ const theme = extendTheme({colors})
 
 function App() {
   return (
-    <Router>
       <ChakraProvider theme={theme}>
-        <Flex minWidth='max-content' alignItems='center' gap='2'>
-          <Box p='2'>
-            <Heading>RESme</Heading>
+        <Flex p='5' border='1px' borderColor='gray.400' borderRadius='0' minWidth='max-content' alignItems='center'>
+          <Box>
+            <Heading>
+              <Link to='/'>RESme</Link> 
+              []
+            </Heading>
           </Box>
+          <Spacer />
+          <ButtonGroup gap='3'>
+            <Button bg='#1E2650' textColor='whiteAlpha.800'>
+              <Link to='/LandingPage'>Landing Page</Link>
+            </Button>
+            <Button bg='#1E2650' textColor='whiteAlpha.800'>
+              <Link to='/signIn'>Sign in</Link>
+            </Button>
+          </ButtonGroup>
         </Flex>
+        
+        <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/LandingPage" element={<LandingPage />}/>
+        <Route path="/signIn" element={<signIn/>}/>
+      </Routes>
       </ChakraProvider>
-    </Router>
+
+  );
+}
+
+//? add home function?
+
+function Home() {
+  return (
+    <ChakraProvider>
+      <Flex p="2">
+        <Box>
+          <Heading>Home</Heading>
+        </Box>
+      </Flex>
+    </ChakraProvider>
   );
 }
 
