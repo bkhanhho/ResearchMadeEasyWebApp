c5253abb6abc78ac43a3eee2618d92e5bbd19958
mackenziehamlett <mackenzie@hamlett.org>
2022-10-21 12:12:51 -0500

    working search

--- frontend/src/components/pages/Main.js
+++ frontend/src/components/pages/Main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../../App.css';
+import Sbar from '../website/Sbar';
+import initialDetails from'../website/initialDetails'
 
 // import Cards from '../Cards';
 // import Footer from '../Footer';
 
 function Main() {
   return (
-    <>
-      
-    </>
+    <div>
+      <Sbar details={initialDetails}/>
+    </div>
   );
 }
 
--- frontend/src/components/pages/user/Account.js
+++ frontend/src/components/pages/user/Account.js
@@ -2,5 +2,7 @@ import React from 'react';
 import '../../../App.css';
 
 export default function Account() {
-  return <h1 className='account'>ACCOUNT</h1>;
+  return (
+    <h1 className='account'>ACCOUNT</h1>
+  );
 }
