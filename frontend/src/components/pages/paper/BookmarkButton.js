c0e2fe4530d2a760f1b09436708037859b24e4ce
bkhanhho <buukhanh.ueh@gmail.com>
2022-11-27 14:52:11 -0600

    npm install @mui/icons-material @mui/material @emotion/styled @emotion/react

--- frontend/src/components/pages/paper/BookmarkButton.js
+++ frontend/src/components/pages/paper/BookmarkButton.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { TiBookmark } from "react-icons/ti";
+import { IoBookmark, IoBookmarkOutline } from "react-icons/io5";
+import Button from "react-bootstrap/Button";
+import { IconButton } from "@mui/material";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
+import { useState } from "react";
 
-function savePaper() {}
 function addPaperToSavedList() {}
 
 export default function BookmarkButton() {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const handleClick = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+  var myLambda = () => handleClick();
   return (
     // TODO: move style to css
-    <span
-      style={{ color: "#27B0FF", fontSize: "40px" }}
-      onClick={() => {
-        savePaper();
-        addPaperToSavedList();
-      }}
-    >
-      {/**TODO: how to use button with icon */}
-      <TiBookmark />
-    </span>
+    <IconButton aria-label="delete" color="primary" onClick={myLambda}>
+      {isBookmarked ? <BookmarkOutlinedIcon /> : <BookmarkBorderOutlinedIcon />}
+    </IconButton>
+
+    // <BookmarkBorderOutlinedIcon />
+    // <BookmarkOutlinedIcon />
+    // <Button
+    //   style={{ color: "#27B0FF" }}
+    //   onClick={() => {
+    //     savePaper();
+    //     addPaperToSavedList();
+    //   }}
+    // >
+    //   {/**TODO: how to use button with icon */}
+    //   <IoBookmark />
+    // </Button>
   );
 }
