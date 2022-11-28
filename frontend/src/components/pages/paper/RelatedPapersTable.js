d92481ea52f9ceff015161d3fc146ec389d00bf7
Evan Smith <evan.c.smith@ttu.edu>
2022-11-27 19:37:05 -0600

    Added list element link

--- frontend/src/components/pages/paper/RelatedPapersTable.js
+++ frontend/src/components/pages/paper/RelatedPapersTable.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { getRelatedPapers } from "../../../services/paper_service";
-// function listFrom(authors) {
-//   if (!authors) return;
-//   return authors.map(({ name }) => `${name}`).join(", ");
-// }
+async function listFrom(title, abstract) {
+    if (!title || !abstract) return;
+    const res = await getRelatedPapers(title, abstract)
+    return res.map(({ paper }) => <li><a href={paper.url}>{paper.score}: {paper.title}</a></li>);
+}
 export default async function RelatedPapersTable({ title, abstract }) {
-
-
-  return (
-    <div>
-       {title}, {abstract}
-       {await getRelatedPapers(title, abstract)}
-    </div>
-  );
+    return (
+        <div>
+            {title}, {abstract}
+            <ul>
+                {await getRelatedPapers(title, abstract)}
+            </ul>
+        </div>
+    );
 }
