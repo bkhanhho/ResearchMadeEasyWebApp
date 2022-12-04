951ef0b5bd8870e048a17be70c7ee82c1f1464da
bkhanhho <buukhanh.ueh@gmail.com>
2022-12-03 23:51:06 -0600

    Commented out broken code

--- frontend/src/components/pages/paper/RelatedPapersTable.js
+++ frontend/src/components/pages/paper/RelatedPapersTable.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import { getRelatedPapers } from "../../../services/paper_service";
-async function listFrom(title, abstract) {
-    if (!title || !abstract) return;
-    const res = await getRelatedPapers(title, abstract)
-    return res.map(({ paper }) => <li><a href={paper.url}>{paper.score}: {paper.title}</a></li>);
-}
-export default async function RelatedPapersTable({ title, abstract }) {
-    return (
-        <div>
-            {title}, {abstract}
-            <ul>
-                {await getRelatedPapers(title, abstract)}
-            </ul>
-        </div>
-    );
+// async function listFrom(title, abstract) {
+//   if (!title || !abstract) return;
+//   const res = await getRelatedPapers(title, abstract);
+//   return res.map(({ paper }) => (
+//     <li>
+//       <a href={paper.url}>
+//         {paper.score}: {paper.title}
+//       </a>
+//     </li>
+//   ));
+// }
+export default function RelatedPapersTable({ title, abstract }) {
+  return <div></div>;
+  //     <div>
+  //         {title}, {abstract}
+  //         <ul>
+  //             {await getRelatedPapers(title, abstract)}
+  //         </ul>
+  //     </div>
+  // );
 }
