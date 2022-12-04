91e58b490c54635618a15732f221eee78ff6f59f
bkhanhho <buukhanh.ueh@gmail.com>
2022-12-04 14:37:29 -0600

    Delete duplicate PaperPreviewCard files in account

--- frontend/src/components/pages/user/account/Cards.js
+++ frontend/src/components/pages/user/account/Cards.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "./Cards.css";
-import PaperPreviewCard from "./PaperPreviewCard";
+import PaperPreviewCard from "../../../website/cards/PaperPreviewCard";
 
 function Cards() {
   return (
